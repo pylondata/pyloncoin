cec5b07ab590267dedbf566cd3764cdb3bfbae5f
Thomas König <tom@fair-coin.org>
2017-04-27 09:15:51 +0200

    rename data directory from `testnet3` to `testnet`

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -46,7 +46,7 @@ public:
     CBaseTestNetParams()
     {
         nRPCPort = 41405;
-        strDataDir = "testnet3";
+        strDataDir = "testnet";
     }
 };
 static CBaseTestNetParams testNetParams;
