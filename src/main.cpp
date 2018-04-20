02977971cf7aacda08513bc2dc2b82ba03be2c21
Thomas König <tom@fair-coin.org>
2018-04-20 11:49:46 +0200

    Allow more sigops in a transaction to enable creation of bigger payloads

--- src/main.cpp
+++ src/main.cpp
@@ -1015,7 +1015,7 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState &state, const C
         // itself can contain sigops MAX_STANDARD_TX_SIGOPS is less than
         // MAX_BLOCK_SIGOPS; we still consider this an invalid rather than
         // merely non-standard transaction.
-        if ((nSigOps > MAX_STANDARD_TX_SIGOPS) || (nBytesPerSigOp && nSigOps > nSize / nBytesPerSigOp))
+        if (nSigOps > MAX_STANDARD_TX_SIGOPS)
             return state.DoS(0, false, REJECT_NONSTANDARD, "bad-txns-too-many-sigops", false,
                 strprintf("%d", nSigOps));
 
