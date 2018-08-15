2649525f706cbb07df01da24090c4235a4f352eb
citrixrep <maxtvstream@live.com>
2018-08-15 18:04:28 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,6 @@ public:
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,223);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
-        vPylonRewardAddress = "fPn9o5z38gA79nf8D1KRMacx4EwnUd2kz8";
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
         fDefaultConsistencyChecks = false;
@@ -346,9 +345,4 @@ void SelectParams(const std::string& network)
 {
     SelectBaseParams(network);
     pCurrentParams = &Params(network);
-}
-
-std::string CChainParams::GetPylonAddressServer() const {
-
-    return vPylonRewardAddress;
 }
