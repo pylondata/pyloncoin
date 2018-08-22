6de97261d26a9c0c83a2f3083c5e5dadca21dfd5
ander7agar <ander7agar@gmail.com>
2018-08-22 18:34:35 +0200

    fix compile.sh

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ cd depends
 make NO_QT=1
 cd ..
 ./autogen.sh
-./configure --disable-tests --disable-bench --with-fasito --with-cvn ..prefix=$PWD/depends/x86_64-pc-linux-gnu
+./configure --disable-tests --disable-bench --with-fasito --with-cvn --prefix=$PWD/depends/x86_64-pc-linux-gnu
 make
