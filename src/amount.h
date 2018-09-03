0a40e8859e77e90a7159e80d85b79b93bbe0da70
ander7agar <ander7agar@gmail.com>
2018-09-03 23:07:34 +0200

    fix max money

--- src/amount.h
+++ src/amount.h
@@ -29,7 +29,7 @@ extern const std::string CURRENCY_UNIT;
  *
  * In Pyloncoin2 this is the exact amount of balances from the Pyloncoin1 block chain
  * */
-static const CAmount MAX_MONEY = 53193831.467966 * COIN; // Maximum number of coins from the Pyloncoin1 blockchain
+static const CAmount MAX_MONEY = 54000000 * COIN; // Maximum number of coins from the Pyloncoin1 blockchain
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /** Type-safe wrapper class for fee rates
